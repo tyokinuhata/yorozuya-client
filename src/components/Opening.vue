d2b87587fb9cf1d4ef524ebcde62a72b82cb7ac3
Kazuki Tobita <kazukiti201@gmail.com>
2017-12-11 04:59:00 +0900

    ランダムした

--- src/components/Opening.vue
+++ src/components/Opening.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>opening</div>
+  <div class="game-wrapper">
+    <div class="message">
+      <div>お客さんが全部で{{ customer }}人やってきました</div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      customer: Math.floor((Math.floor(Math.random() * (20 - 10) + 10) * (1 + localStorage.getItem('population') / 10)))
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.game-wrapper {
+  height: 100%;
+  position: relative;
+}
+
+.message {
+  position: absolute;
+  bottom: 1rem;
+  width: 90%;
+  margin: 0 2%;
+  padding: 2rem 1rem;
+  border: 1px solid #fff;
+  border-radius: 4px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #fff;
+}
+</style>
