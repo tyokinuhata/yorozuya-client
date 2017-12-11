b1fb15a4048f7abfc9854e240f9101252e47b7c9
ahaha0807 <b5331@oic.jp>
2017-12-11 22:26:39 +0900

    リファクタリング

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="game-wrapper">
-    <img :style="{ backgroundImage: `url('${path}')` }" alt="">
+    <img alt="">
     <div class="message">
       {{ name }}が{{ amount }}個売れて{{ price }}Gになりました。
     </div>
@@ -8,84 +8,88 @@
 </template>
 
 <script>
-import axios from 'axios'
+  import axios from 'axios'
 
-import util from '../util/Util.js'
+  import util from '../util/Util.js'
 
-export default {
-  data() {
-    return {
-      message: '',
-      name: '',
-      amount: 0,
-      price: 0,
-      path: ''
-    }
-  },
-  methods: {
-    sliceBtc(data) {
-      let str = String(data)
-      let arr = []
-      arr.push(str.slice(0, 3))
-      for (let i = 3; i < 7; i++) {
-        arr.push(str.slice(i, i + 1))
+  export default {
+    data() {
+      return {
+        message: '',
+        name: '',
+        amount: 0,
+        price: 0,
+        path: ''
       }
-      return arr
-    }
-  },
-  created() {
-    let data
-    axios.get('/api/price')
-      .then(response => {
-        data = this.sliceBtc(response.data.price)
-        data = data.map(function(element) {
-          return Number(element)
-        })
-        let price = data[0]
-        for (let i = 0; i < 5; i++) {
-          price += data[i]
+    },
+    methods: {
+      sliceBtc(data) {
+        let str = String(data)
+        let arr = []
+        arr.push(str.slice(0, 3))
+        for (let i = 3; i < 7; i++) {
+          arr.push(str.slice(i, i + 1))
         }
-        let product = Math.floor(Math.random() * 45)
-        let amount = Math.floor(Math.random() * (10 - 5) + 5)
-        this.name = util.image.items[product].name
+        return arr
+      }
+    },
+    created() {
+      let data
 
-        this.imgPath = require(util.image.items[product].path)
-        console.log(require(util.image.items[product].path))
-        this.amount = amount
-        this.price = price * amount * util.image.items[product].price
-        this.path = util.image.items[product].path
-        location.reload()
-      })
-      .catch(error => {
-        console.log(error)
-      })
+      let product = Math.floor(Math.random() * 45)
+      this.imgPath = util.image.items[product].path
+      this.name = util.image.items[product].name
+      this.price = this.price * util.image.items[product].price
+      this.path = util.image.items[product].path
+
+      axios.get('/api/price')
+        .then(response => {
+          data = this.sliceBtc(response.data.price)
+
+          data = data.map(function (element) {
+            return Number(element)
+          })
+
+          let price = data[0]
+          for (let i = 0; i < 5; i++) {
+            price += data[i]
+          }
+
+          this.amount = Math.floor(Math.random() * (10 - 5) + 5)
+          this.price = this.price * this.amount
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
   }
-}
+
 </script>
 
 <style lang="scss" scoped>
-.game-wrapper {
-  height: 100%;
-  position: relative;
-}
+  .game-wrapper {
+    height: 100%;
+    position: relative;
+  }
 
-.message {
-  position: absolute;
-  bottom: 1rem;
-  width: 90%;
-  margin: 0 2%;
-  padding: 2rem 1rem;
-  border: 1px solid #fff;
-  border-radius: 4px;
-  font-size: 1.2rem;
-  font-weight: bold;
-  color: #fff;
-}
+  .message {
+    position: absolute;
+    bottom: 1rem;
+    width: 90%;
+    margin: 0 2%;
+    padding: 2rem 1rem;
+    border: 1px solid #fff;
+    border-radius: 4px;
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: #fff;
+  }
 
-.image img{
-  width:300px;
-  height: 300px;
-  margin: 0 auto;
-  display: block;
-}
+  .image img {
+    width: 300px;
+    height: 300px;
+    margin: 0 auto;
+    display: block;
+  }
+
 </style>
